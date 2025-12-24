efd8252f4243c1d2fa7599bdde85e4231bb95442
Dipin003 <127880140+Dipin003@users.noreply.github.com>
2025-12-24 15:23:58 +0530

    Implement solution for kids with greatest candies

---
+++ KidsWithGreatestNumberofCandies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> res = new ArrayList<>();
+
+        int max = 0;
+        for (int c : candies) {
+            max = Math.max(max, c);
+        }
+
+        for (int c : candies) {
+            res.add(c + extraCandies >= max);
+        }
+
+        return res;
+    }
+}
