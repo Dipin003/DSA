de4dc310976f796d69c6cebb44f31ad061cd195c
Dipin003 <127880140+Dipin003@users.noreply.github.com>
2025-12-29 15:35:52 +0530

    Implement reverse linked list function

---
+++ ReverseLinkedList.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        prev.next = null; 
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev; 
+    }
+}
